5ef83bf648246c7c6a0cb9a05a95103aeaf31810
Jascha Rafael Franke <hola@11once.de>
2018-09-24 20:05:41 +0200

    Final style change for avatar homepage

--- app/javascript/components/popup.js
+++ app/javascript/components/popup.js
@@ -1,66 +1,66 @@
-import "jquery-confirm"
+// import "jquery-confirm"
 
-function SendEmail () {
-  var button = $("#email")
-    // console.log(button.data('saved'));
-    button.confirm({
-      columnClass: 'col-md-4 col-md-offset-4',
-      // boxWidth: '30%',
-      useBootstrap: false,
-      // icon: 'fa fa-spinner fa-spin',
-      escapeKey: 'Ok',
-      backgroundDismiss: true,
-      title: 'Email',
-      content: `<strong>hola@11once.de</strong>`,
-      theme: 'modern',
-      buttons: {
-          Ok: {
-              text: 'Ok',
-              // action: function(){
-              //     // window.scrollTo({
-              //     //   top: 0,
-              //     //   behavior: "smooth"
-              //     // });
-              // }
-          },
-        },
-      type: 'blue'
-    });
-};
-
-
-export { SendEmail };
-
-function ShowPhoneNu () {
-  var button = $("#phone")
-    // console.log(button.data('saved'));
-    button.confirm({
-      columnClass: 'col-md-4 col-md-offset-4',
-      // boxWidth: '30%',
-      useBootstrap: false,
-      // icon: 'fa fa-spinner fa-spin',
-      escapeKey: 'Ok',
-      backgroundDismiss: true,
-      title: 'Phone',
-      content: `<strong>+49 151 1577 6592</strong>`,
-      theme: 'modern',
-      buttons: {
-          Ok: {
-              text: 'Ok',
-              // action: function(){
-              //     // window.scrollTo({
-              //     //   top: 0,
-              //     //   behavior: "smooth"
-              //     // });
-              // }
-          },
-        },
-      type: 'blue'
-    });
-};
+// function SendEmail () {
+//   var button = $("#email")
+//     // console.log(button.data('saved'));
+//     button.confirm({
+//       columnClass: 'col-md-4 col-md-offset-4',
+//       // boxWidth: '30%',
+//       useBootstrap: false,
+//       // icon: 'fa fa-spinner fa-spin',
+//       escapeKey: 'Ok',
+//       backgroundDismiss: true,
+//       title: 'Email',
+//       content: `<strong>hola@11once.de</strong>`,
+//       theme: 'modern',
+//       buttons: {
+//           Ok: {
+//               text: 'Ok',
+//               // action: function(){
+//               //     // window.scrollTo({
+//               //     //   top: 0,
+//               //     //   behavior: "smooth"
+//               //     // });
+//               // }
+//           },
+//         },
+//       type: 'blue'
+//     });
+// };
 
 
-export { ShowPhoneNu };
+// export { SendEmail };
+
+// function ShowPhoneNu () {
+//   var button = $("#phone")
+//     // console.log(button.data('saved'));
+//     button.confirm({
+//       columnClass: 'col-md-4 col-md-offset-4',
+//       // boxWidth: '30%',
+//       useBootstrap: false,
+//       // icon: 'fa fa-spinner fa-spin',
+//       escapeKey: 'Ok',
+//       backgroundDismiss: true,
+//       title: 'Phone',
+//       content: `<strong>+49 151 1577 6592</strong>`,
+//       theme: 'modern',
+//       buttons: {
+//           Ok: {
+//               text: 'Ok',
+//               // action: function(){
+//               //     // window.scrollTo({
+//               //     //   top: 0,
+//               //     //   behavior: "smooth"
+//               //     // });
+//               // }
+//           },
+//         },
+//       type: 'blue'
+//     });
+// };
+
+
+// export { ShowPhoneNu };
 
 // function CopyToClipboard() {
 //   /* Get the text field */
